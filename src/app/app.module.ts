4a94a878d50daf783aac4f8298345ab72b510d71
LEJMI <heithem.lejmi@sogeti.com>
2019-07-11 22:14:43 +0200

    Add the two ways binding by adding the input feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
 import { AppareilComponent } from './appareil/appareil.component';
@@ -12,7 +12,8 @@ import { AppareilComponent } from './appareil/appareil.component';
     AppareilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
