110c280035a42d2438158416be86af574cb76ed4
LEJMI <heithem.lejmi@sogeti.com>
2019-07-12 12:00:33 +0200

    Add a custom property appareilName

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-appareil',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appareil.component.css']
 })
 export class AppareilComponent implements OnInit {
-  appareilName = 'Machine à laver';
+  @Input() appareilName: string;
   appareilStatus = 'éteint';
   constructor() { }
 
