7c6fe519d84b5759dc19d0669b8f321e6e46ca95
LEJMI <heithem.lejmi@sogeti.com>
2019-07-14 21:08:59 +0200

    Add the first routes : appareil-view and auth, in the app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+// Components
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
 import { AppareilComponent } from './appareil/appareil.component';
+
 // Services
 import { AppareilService } from './service/appareil.service';
-import { AuthComponent } from './auth/auth.component';
-import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 
+// Routers
+import {Routes, RouterModule} from '@angular/router';
+
+const appRoutes: Routes = [
+{path: 'appareils', component: AppareilViewComponent},
+{path: 'auth', component: AuthComponent},
+{path: '', component: AppareilViewComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +29,8 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AppareilService],
   bootstrap: [AppComponent]
--- src/app/appareil-view/appareil-view.component.ts
+++ src/app/appareil-view/appareil-view.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+// Services
+import { AppareilService } from '../service/appareil.service';
 @Component({
   selector: 'app-appareil-view',
   templateUrl: './appareil-view.component.html',
   styleUrls: ['./appareil-view.component.css']
 })
 export class AppareilViewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  appareils: any[];
+  isAuth = false;
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
   }
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+}
+  onAllumer() {
+    this.appareilService.switchOnAll();
+  }
+  onEteindre() {
+    if (confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
+      this.appareilService.switchOffAll();
+    } else {
+      return null;
+    }
+}
 
 }
