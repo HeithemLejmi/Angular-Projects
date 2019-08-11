2b8210e836c2a8d782d8c574c88cff1e4cb20d08
LEJMI <heithem.lejmi@sogeti.com>
2019-08-11 21:01:04 +0200

    add an instance of Router to the auth-guard service so as to use the method navigate

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService, private router: Router){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
